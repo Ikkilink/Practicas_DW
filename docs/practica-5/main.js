eb577e0f356389612185bc87e58b1bd09f9595c4
Carol Torres <ikkilink@hotmail.com>
2024-09-17 18:44:56 -0600

    carrito

--- docs/practica-5/main.js
+++ docs/practica-5/main.js
@@ -1,12 +1,11 @@
 let productos = [
     {nombre: "Camiseta", precio: 15, stock: 10},
-    {nombre: "Pantalon", precio: 10, stock: 20},
+    {nombre: "Pantalon", precio: 50, stock: 20},
     {nombre: "Falda", precio: 20, stock: 5},
 ];
 //2
 let carrito = [];
-function agregarCarrito(productoNombre, cantidad)
-{
+function agregarCarrito(productoNombre, cantidad){
 for (let producto of productos) {
     if (producto.nombre === productoNombre)
     {
@@ -31,6 +30,29 @@ for (let producto of productos) {
 }
 console.error(`El producto "${productoNombre}" no existe`);
 }
+//3
+function calcularTotal(){
+    let total = 0;
+    for (let item of carrito) {
+        total += item.precio*item.cantidad;
+    }
+    return total;
+}
+function aplicarDescuento(){
+    if(total>= 100)
+    {
+        return total*.9;
+    }
+}
+function procesarcompra(){
+    console.log("Procesando compra . . .")
+    setTimeout(function(){
+         let total = calcularTotal();
+         total = aplicarDescuento();
+         console.log(`Su total es de $(total)`);
+    },3000);
+}
 agregarCarrito("Pantalon",3);
 agregarCarrito("Pantalon",3);
-agregarCarrito("Pantalon",20);
+agregarCarrito("Pantalon",3);
+calcularTotal();
