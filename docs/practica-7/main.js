5166a29297180f5093bee4255718ff61011a2f75
AZUL13\Azul3 <dbslafbdlahj>
2024-10-03 18:44:54 -0600

    uwu

--- docs/practica-7/main.js
+++ docs/practica-7/main.js
@@ -13,32 +13,65 @@ const $errorsMessages = d.querySelectorAll(".error");
 
 // Función de Validación del Formulario
 function validateForm(e) {
-    let isValid = true;
-    if($nameInput.value.trim()==="")
-    {
-        $nameError.innerText = "El nombre es obligatorio";
-        isValid=false;
-    }
+  // Reiniciar mensajes de error y éxito
+  $errorsMessages.forEach((el) => {
+    el.innerText = "";
+  });
+  $successMessage.innerText = "";
 
-    if($emailInput.value.trim()==="")
-    {
-        $emailError.innerText = "El email es obligatorio";
-        isValid=false;
-    }
-    if($passwordInput.value.trim()==="")
-    {
-         $passwordError.innerText = "La contraseña es obligatoria";
-            isValid=false;
-    }
-    if(!isValid){
-        e.preventDefault();
-    }
-    else{
-        e.preventDefault();
-        $successMessage.innerText = "formulario enviado exitosamente"
-        $form.reset();
-    }
+  let isValid = true;
+
+  //Validar Nombre
+  let namepatter = /^[A-Za-z\s]+$/;
+  if ($nameInput.value.trim() === "") {
+    $nameError.innerText = "El nombre es obligatorio";
+    isValid = false;
+  }else if(!namepatter.test($nameInput.value.trim())){
+    $nameError.innerText = "Solo se aceptan letras y espacion en blanco";
+    isValid = false;
+  }
+
+  //Validar Email
+  let emailPattern = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+  if ($emailInput.value.trim() === "") {
+    $emailError.innerText = "El email es obligatorio";
+    isValid = false;
+  } else if (!emailPattern.test($emailInput.value.trim())) {
+    $emailError.innerText = "El formato del correo no es válido";
+    isValid = false;
+  }
+
+  //Validar Password
+  let passwordpattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}:"<>?[\];',./`~\\-]).{8,}$/;
+  if ($passwordInput.value.trim() === "") {
+    $passwordError.innerText = "La contraseña es obligatorio";
+    isValid = false;
+  } else if ($passwordInput.value.trim().length < 8) {
+    $passwordError.innerText = "La contraseña debe tener al menos 8 caracteres";
+    isValid = false;
+  }else if(!passwordpattern.test($passwordInput.value.trim()))
+  {
+    $passwordError.innerText = "El formato de contraseña no es valido";
+    isValid = false;
+  }
+
+  //Validar Confirmar Password
+  if ($confirmPasswordInput.value.trim() !== $passwordInput.value.trim()) {
+    $confirmPasswordError.innerText = "Las contraseñas no coinciden";
+    isValid = false;
+  }
+
+  if (!isValid) {
+    //Prevenir el envío del formulario si hay errores
+    e.preventDefault();
+  } else {
+    e.preventDefault();
+    $successMessage.innerText = "Formulario enviado exitosamente.";
+    $form.reset();
+    // Aquí puedes manejar el envío real de datos a un servidor, por ejemplo, usando fetch.
+  }
 }
 
 $form.addEventListener("submit", validateForm);
+
 
